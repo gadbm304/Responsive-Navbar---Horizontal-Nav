2bc6de3b3bcd0410f7d4139d1324b79158b6806b
gadbm304 <gadbm304@hsd.k12.or.us>
2022-05-18 14:11:23 -0700

    nav bar animation

---
+++ scripts.js
@@ -0,0 +1,39 @@
+/* When the Page loads, add an open class to nav if the screen is wide enough. */ 
+var mobileBreakpoint = 440; 
+
+window.onload = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    // add 'open' class to the ul in the navbar if wider than the breakpoint
+    if (screenWidth > mobileBreakpoint) {
+     globalNavbar.classList.add("open");
+     toggle.classList.add("open");
+    }
+}
+
+// add a toggle click function, which serves as an on-off button
+toggle.onclick = function() {
+    if (globalNavbar.classList.contains("open")) {
+        globalNavbar.classList.remove("open");
+    }
+    else {
+        globalNavbar.classList("open");
+    }
+}
+
+window.onresize = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+        toggle.classList.add("open");
+    }
+    else {
+        globalNavbar.classList.remove("open");
+        toggle.classList.remove("open");
+    }
+}   
