e9065d80b8a0e775eb2bd9eff946e24a3e69b4b2
gadbm304 <gadbm304@hsd.k12.or.us>
2022-05-18 15:14:20 -0700

    fixing toggle

--- scripts.js
+++ scripts.js
@@ -6,20 +6,17 @@ window.onload = function() {
     var globalNavbar = document.getElementById("global-nav");
     var toggle = document.getElementById("menu-toggle");
 
-    // add 'open' class to the ul in the navbar if wider than the breakpoint
+// add 'open' class to the ul in the navbar if wider than the breakpoint
     if (screenWidth > mobileBreakpoint) {
      globalNavbar.classList.add("open");
      toggle.classList.add("open");
     }
-}
 
-// add a toggle click function, which serves as an on-off button
-toggle.onclick = function() {
-    if (globalNavbar.classList.contains("open")) {
-        globalNavbar.classList.remove("open");
-    }
-    else {
-        globalNavbar.classList("open");
+
+    // add a toggle click function, which serves as an on-off button
+    toggle.onclick = function() {
+        globalNavbar.classList.toggle("open");
+        toggle.classList.toggle("open");
     }
 }
 
